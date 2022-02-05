caf26c5142d77410c04dbff8547f85b6fdffc2e0
JoseFredes7 <94748095+JoseFredes7@users.noreply.github.com>
2022-02-05 15:19:31 -0300

    update

---
+++ src/main/java/com/coding/dojo/projecto/service/impl/ArticleServiceImpl.java
@@ -0,0 +1,10 @@
+package com.coding.dojo.projecto.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.coding.dojo.projecto.services.ArticleService;
+
+@Service
+public class ArticleServiceImpl implements ArticleService {
+
+}
