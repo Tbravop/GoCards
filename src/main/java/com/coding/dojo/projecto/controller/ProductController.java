fc1087f44703bd42f075fccb70a7e6dbb820eed1
JoseFredes7 <94748095+JoseFredes7@users.noreply.github.com>
2022-02-09 11:23:47 -0300

    update

--- src/main/java/com/coding/dojo/projecto/controller/ProductController.java
+++ src/main/java/com/coding/dojo/projecto/controller/ProductController.java
@@ -1,13 +1,9 @@
 package com.coding.dojo.projecto.controller;
 
-import java.security.Principal;
-import java.util.List;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,25 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.coding.dojo.projecto.model.Product;
-import com.coding.dojo.projecto.model.User;
 import com.coding.dojo.projecto.services.CategoryService;
 import com.coding.dojo.projecto.services.ProductService;
-import com.coding.dojo.projecto.services.RoleService;
-import com.coding.dojo.projecto.services.UserService;
-import com.coding.dojo.projecto.validator.UserValidator;
 
-@Controller
-public class MarketController {
+public class ProductController {
 
-	@Autowired
-	@Lazy
-	private UserService userService;
-	@Autowired 
-	@Lazy
-	private UserValidator userValidator;
-	@Autowired 
-	@Lazy
-	private RoleService roleService;
 	@Autowired
 	@Lazy
 	private ProductService productService;
@@ -45,43 +27,6 @@ public class MarketController {
 	@Lazy
 	private CategoryService categoryService;
 	
-	@GetMapping("/registration")
-    public String register(@Valid @ModelAttribute("user")User user, Model model) {
-        model.addAttribute("rol", roleService.allRole());
-        return"loginRegister.jsp";
-    }
-	@PostMapping("/registration")
-    public String registerUser(@Valid @ModelAttribute("user")User user, BindingResult result) {
-        userValidator.validate(user, result);
-        if(result.hasErrors()) {
-            return"loginRegister.jsp";
-        }
-        else {
-            userService.createUser(user);
-            return"redirect:/registration";
-        }
-	}
-	@RequestMapping("/login")
-    public String login(@RequestParam(value="error", required= false)String error, @RequestParam(value="logout", required=false)String logout,Model model) {
-        if(error != null) {
-            model.addAttribute("errorMessage", "Error o Contraseña Erroneo, Intente de nuevo.");
-        }
-        if(logout != null) {
-            model.addAttribute("logout", "Se cerro Su session de forma Exitosa!");
-            return"redirect:/registration";
-        }
-        else {
-            return"redirect:/";
-        }
-        
-    }
-	
-	@GetMapping("/")
-	public String index(Principal principal, Model model) {
-		List<Product> product = productService.allProduct();
-		model.addAttribute("producto", product);
-		return "index.jsp";
-	}
 	@GetMapping("/crear")
 	public String newProd(@ModelAttribute("product") Product product) {
 		return "nuevoProducto.jsp";
