f5a6acdb26193b8007bbe85c2899b482de7a7dcf
Javiera Benitez <benitezj.pradenas@gmail.com>
2022-02-09 18:12:55 -0300

    update

--- src/main/java/com/coding/dojo/projecto/controller/SellController.java
+++ src/main/java/com/coding/dojo/projecto/controller/SellController.java
@@ -1,7 +1,9 @@
 package com.coding.dojo.projecto.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class SellController {
 
 	@GetMapping("/carrito")
