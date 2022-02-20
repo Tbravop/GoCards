81c31c70d919afe196d443df52e8ee3b2abace7f
dane-irene <94883628+dane-irene@users.noreply.github.com>
2022-02-20 17:20:21 -0300

    update

--- src/main/java/com/coding/dojo/projecto/controller/SellController.java
+++ src/main/java/com/coding/dojo/projecto/controller/SellController.java
@@ -10,19 +10,9 @@ public class SellController {
 	public String carrito() {
 		return "cart.jsp";
 	}
+	
+	@GetMapping("/envio")
+	public String envio() {
+		return "envio.jsp";
+	}
 }
-
-// boton corazon para agregar a favoritos
-
-// boton eliminar para sacar un producto del carro
-
-// revisar si se puede cambiar dropdown de cantidad de productos por uno que aumente o disminuya la cantidad con signos + o - (front)
-
-
-// hacer que el total sume los valores de las cartas a comprar
-
-//ignorar el descuento por ahora
-
-// boton comprar, debe redirigir al envio (a un formulario para la otra pagina)
-
-//boton continuar comprando debe redirigir a marketplace
